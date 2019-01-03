0dbf709facaff3f432f33cf9f61ecce134a93982
MattFulton <matt_fulton@ymail.com>
2019-01-02 20:54:59 -0500

    V2

--- index.php
+++ index.php
@@ -1,34 +1,6 @@
 <?php
-$title = 'Tradelands Exchange';
-$content = "<h1>Welcome to the Tradelands Exchange.</h1>
-            <h4>Click MARKET to begin browsing.</h6>
-            <br>
-            <h2>Rules</h2>
-            <ol>
-                <li>You may not charge more for an item than you list it for.</li>
-                <li>Do not make joke pizza.</li>
-                <li>Do not post items for sale that are available ingame for doubloons.</li>
-            </ol>
-            <br>
-            <h3>How to Make an Account</h3>
-            <p>Enter a private server, and once you've spawned, say '/tlx PASSWORD' where PASSWORD indicates were your password will be.
-            Make your password something that cannot be easily guessed and <b>DO NOT</b> set your password to your Roblox password.
-            You can use this command again later to change your password.</p>
-
-
-            <h3>How to Post Items for Sale</h3>
-
-            <p>Once your account has been made, and while you're in a private server, use '/tlx' to enable the 'Post to TLX' option when selecting an item in your inventory.
-            Using this will post the item for sale on the website.  Players may then contact you ingame or on Discord to buy the item.  Posting the same item again will update the listing.</p>
-
-            <p>To post a set, equip the items on your character and say '/tlxset'.  This will post all items that you are wearing which have matching materials.
-            A set must contain at least both hip items and a back item.  Pocket items will be included as well if they are equipped and match.</p>
-
-            <p>To remove a listing, log in on the website and go to the MY ITEMS page.</p>
-
-            <p>Try to keep your listings as accurate and up-to-date as possible.</p>
-
-            ";
+$title = 'Test Site';
+$content = "AHHHHHHH";
 
 include 'template.php';
 
